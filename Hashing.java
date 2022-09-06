9acf385dc2015d696d7e0ce593369f5a77ca24a6
deepalis0 <36595560+deepalis0@users.noreply.github.com>
2022-09-06 23:14:04 +0530

    Hashing

---
+++ Hashing.java
@@ -0,0 +1,57 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Hashing {
+
+    public static void main(String []args)
+    {
+       HashMap<String,Integer> colorMap = new HashMap<>();
+       colorMap.put("RED",1);
+       colorMap.put("YELLOW",2);
+       colorMap.put("BLUE",3);
+
+       //update the color code of RED
+       colorMap.put("RED",4);
+
+       System.out.println("The code for BLUE is "+ colorMap.get("BLUE"));
+       System.out.println("The code for RED is "+ colorMap.get("RED"));
+
+       if(colorMap.containsKey("GREEN"))
+         System.out.println("The code for GREEN is "+ colorMap.get("GREEN"));
+
+       int codeGREEN = colorMap.getOrDefault("GREEN",0);
+       int codeRED   = colorMap.getOrDefault("RED",0);
+       System.out.println(codeGREEN+" "+codeRED);
+
+       //If code for a colour DOES NOT exist, please provide default value = 0
+//       int x = colorMap.get("GREEN"); -->ERROR as NPE
+//       int y =x+3;
+//       System.out.println(x);
+
+        //ITERATION
+        /*
+        Iterating over array:
+            int[] arr = new int[]{1,2,3,4};
+            for(int a : arr)
+            {
+                System.out.println(a);
+            }
+         */
+
+        //row in a HashMap  KEY VALUE  -- ENTRY in a HASHMAP
+        //entry - KEY and VALUE
+        for (Map.Entry<String,Integer> entry : colorMap.entrySet()) {
+            System.out.println(entry);
+            System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+        }
+
+        // using keySet() for iteration over keys
+        for (String Colorname : colorMap.keySet())
+            System.out.println("key: " + Colorname);
+
+        // using values() for iteration over values
+        for (Integer code : colorMap.values())
+            System.out.println("value: " + code);
+    }
+
+}
