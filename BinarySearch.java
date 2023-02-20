ac08f27151d328d0867eb05e0dc17a66e94e9b88
deepalishinde <deepalis0@gmail.com>
2023-02-20 16:33:51 -0500

    BinarySe

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,7 +1,10 @@
+import java.util.Arrays;
 public class BinarySearch {
     public static void main(String args[]) {
 
         int[] a = new int[]{4,1,3,9,7}; //{10, 80, 30, 90, 40, 50, 70}; // {10,9,8};
+        //1 3 4 9 7
+        Arrays.sort(a);
         int target = 3;
         int index = binarySearch(a,0,a.length-1,target);    //iterativeBinarySearch(a,0,a.length-1,target);
 
@@ -11,13 +14,15 @@ public class BinarySearch {
     }
     static int binarySearch(int[] a,int low,int high,int target)
     {
-        if(low<high)
+        if(low<=high)
         {
-           int mid = low + (high-low)/2;
+           int mid = low + (high-low)/2;// (high+low)/2
            if(target==a[mid])
            {
                return mid;
            }
+           // 1 3 5 19 30 45    target =3
+            //a[mid] = 19 mid=3
            if(a[mid]>target)
            {
                return binarySearch(a,low,mid-1,target);
@@ -36,11 +41,11 @@ public class BinarySearch {
                 return mid;
             else if(target<a[mid])
             {
-                high--;
+                high = mid - 1;
             }
             else
             {
-                low++;
+                low = mid+1;
             }
         }
         return -1;
