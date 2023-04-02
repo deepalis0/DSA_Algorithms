aae48ea9b1af4d2805d7a3c2572be0244a27c66f
deepalishinde <deepalis0@gmail.com>
2023-04-02 17:12:01 -0400

    Bubble Sort

---
+++ BubbleSort.java
@@ -0,0 +1,33 @@
+public class BubbleSort {
+
+    void sort(int[] a,int n)
+    {
+        boolean isSwapped = true;
+        for(int i=0;i<n-1;i++)//n-1 passes
+        {
+            if(isSwapped) {
+                isSwapped = false;
+                for (int j = 0; j < n - 1 - i; j++) {
+                    if (a[j] > a[j + 1]) {
+                        //swap
+                        isSwapped = true;
+                        int temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+    public static void main(String[] args)
+    {
+        int[] a = new int[]{12,11,13,5,6};
+        BubbleSort bubbleSort = new BubbleSort();
+        bubbleSort.sort(a,a.length);
+        for(int i=0;i<a.length;i++)
+        {
+            System.out.print(a[i]+" ");
+        }
+    }
+
+}
